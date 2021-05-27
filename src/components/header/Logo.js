7c5ee0f911501c0e93eee11cedfa11630f2c86fe
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2021-05-27 20:25:54 +0700

    feat: WIP: Header component skeleton (#7)

---
+++ src/components/header/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import deuvoxLogo from "../../assets/img/deuvox-logo-black.png";
+
+const Logo = () => {
+  return (
+    <div>
+      <img alt="deuvox-logo" src={deuvoxLogo} />
+    </div>
+  );
+};
+
+export default Logo;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
 
-import App from './App';
+import App from "./App";
 
-import 'antd/dist/antd.css';
+import "./assets/scss/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
