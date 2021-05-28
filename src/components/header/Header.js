5fab2a107547e0ee12680134c9f4752a6af98bd1
FadellMuhammad <fadel54031@gmail.com>
2021-05-28 23:25:37 +0700

    finish header compenent, some problems with icon size small and blur, and not responsive.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Col, Input, Row } from "antd";
 import Logo from "./Logo";
+import lonceng from "../../assets/img/lonceng.png";
+import product from "../../assets/img/product.png";
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
 import "./Header.scss";
 
@@ -8,23 +12,37 @@ const { Search } = Input;
 
 const Header = () => {
   return (
-    <header id="header" className="container">
-      <Row align="middle" justify="space-between">
-        <Col span={4}>
-          <Logo />
-        </Col>
-        <Col span={12}>
-          <Search
-            placeholder="Search for good things"
-            style={{ borderRadius: 8, width: 515 }}
-          />
-        </Col>
-        <Col span={4}>
-          <p>Profile goes here</p>
-        </Col>
-      </Row>
-    </header>
-    
+    <header id="header">
+      <div className="container">
+        <Row align="middle">
+          <Col span={4}>
+            <Logo />
+          </Col>
+          <Col span={10}>
+            <Search id="search"
+              placeholder="Search for good things ..."
+              style={{ width: 483 }}
+            />
+          </Col>
+          <Col span={4}>
+            <div className="icon">
+              <img alt="lonceng" src={product} id="product" />
+              <img alt="product" src={lonceng} id="lonceng" />
+              <div id="notif"></div>
+            </div>
+          </Col>
+          <Col span={1}>
+            <div id="line"></div>
+          </Col>
+          <Col span={4}>
+            <div className="profile">
+              <Avatar size={48} icon={<UserOutlined />} id="avatar" />
+              <p>Fadel Muhammad</p>
+            </div>
+          </Col>
+        </Row>
+      </div >
+    </header >
   );
 };
 
