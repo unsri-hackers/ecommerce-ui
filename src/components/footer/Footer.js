9bdaa510e6d043f2f5949b35bd873dc355d25472
fadelarmando <fadelarmando3@gmail.com>
2021-05-30 22:07:47 +0700

    feat: add footer component (#9)

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,61 @@
 import React from "react";
-import { Layout } from 'antd';
+import { Col, Input, Row } from "antd";
+import Logo from "./Logo";
 
-const { Footer: AntFooter } = Layout;
+import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <AntFooter style={{ textAlign: 'center' }}>
-      Copyright &copy; {new Date().getFullYear()}
-      <br />
-      Made with ❤️ by deuvox
-    </AntFooter>
+    <footer id="footer">
+      <div className="container">
+        <div className="footer-content">
+        <Row align="middle">
+          <Col span={6}>
+            <div className="footer-col-logo">
+              <Logo />
+              <p>Lorem ipsum dolor sit amet Lorem ipsum dolor elit</p>
+            </div>
+          </Col>
+          <Col span={6}>
+            <div className="footer-col">
+              <h4 className="title">Lorem, ipsum</h4>
+              <ul>
+                <li>Dolor sit amet</li>
+                <li>Constect elit</li>
+                <li>Payments</li>
+              </ul></div>
+          </Col>
+          <Col span={6}>
+            <div className="footer-col">
+              <h4 className="title">Explore Us</h4>
+              <ul>
+                <li>About</li>
+                <li>Privacy Policy</li>
+                <li>Terms & Conditions.</li>
+              </ul>
+            </div>
+          </Col>
+          <Col span={6}>
+            <div className="footer-col">
+              <h4 className="title">Get in Touch</h4>
+              <ul>
+                <li>support@deuvox.com</li>
+                <li>0711 - 7902 - 0382</li>
+                <li>Deuvox, Palembang</li>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+        </div>
+        <Row>
+          <Col span={24}>
+            <div className="footer-bottom">
+              <p>Copyright 2021 &bull; All rights reserved &bull; Deuvox</p>
+            </div>
+          </Col>
+        </Row>
+      </div >
+    </footer>
   );
 };
 
---
+++ src/components/footer/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import deuvoxLogo from "../../assets/img/deuvox-logo-black.png";
+
+const Logo = () => {
+  return (
+    <div id="logo">
+      <img alt="deuvox-logo" src={deuvoxLogo} />
+    </div>
+  );
+};
+
+export default Logo;
