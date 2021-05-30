ef5a4adb44abd891406635761944e6150a15d6c4
LordAldi <aldianugra09@gmail.com>
2021-05-30 15:16:37 +0700

    feat: impelement costumizable theme and add few basic variable (#16)

---
+++ craco.config.js
@@ -0,0 +1,24 @@
+const CracoLessPlugin = require("craco-less");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              "@primary-color": "#FDE54E",
+              "@success-color": "#1AC040",
+              "@error-color": "#D3351D",
+              "@btn-primary-color": "#323232",
+              "@text-color": "#323232",
+              "@text-color-secondary": "#8893A9",
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
