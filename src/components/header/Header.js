861661c4a0dade3b7cbd8b720030f20d6148736a
FadellMuhammad <fadel54031@gmail.com>
2021-05-31 21:56:20 +0700

    update revisi

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Col, Input, Row } from "antd";
 import Logo from "./Logo";
-import lonceng from "../../assets/img/lonceng.png";
-import product from "../../assets/img/product.png";
+import notificationIcon from "../../assets/img/notificationIcon.png";
+import addNewProduct from "../../assets/img/add-new-product.png";
 import { Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
+import { Badge } from 'antd';
 
 import "./Header.scss";
 
@@ -15,34 +16,42 @@ const Header = () => {
     <header id="header">
       <div className="container">
         <Row align="middle">
-          <Col span={4}>
+
+          <Col xs={9} sm={7} md={4} lg={4}>
             <Logo />
           </Col>
-          <Col span={10}>
+
+          <Col xs={0} sm={0} md={6} lg={9}>
             <Search id="search"
               placeholder="Search for good things ..."
-              style={{ width: 483 }}
             />
           </Col>
-          <Col span={4}>
+
+          <Col xs={8} sm={7} md={6} lg={5}>
             <div className="icon">
-              <img alt="lonceng" src={product} id="product" />
-              <img alt="product" src={lonceng} id="lonceng" />
-              <div id="notif"></div>
+              <Badge>
+                <img alt="add-new-product" src={addNewProduct} id="addNewProduct" />
+              </Badge>
+              <Badge count={0} showZero color="yellow">
+                <img alt="notificationIcon" src={notificationIcon} id="notificationIcon" />
+              </Badge>
             </div>
           </Col>
-          <Col span={1}>
+
+          <Col xs={1} sm={1} md={1} lg={1}>
             <div id="line"></div>
           </Col>
-          <Col span={4}>
+
+          <Col xs={6} sm={9} md={7} lg={5}>
             <div className="profile">
               <Avatar size={48} icon={<UserOutlined />} id="avatar" />
               <p>Fadel Muhammad</p>
             </div>
           </Col>
+
         </Row>
-      </div >
-    </header >
+      </div>
+    </header>
   );
 };
 
