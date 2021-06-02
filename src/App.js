9c3c7ca2da71d225b8b173cf6fb1d600944865ee
LordAldi <aldianugra09@gmail.com>
2021-06-02 18:11:38 +0700

    bugfix: remove react-router-dom change to @react/router (#22)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { Home, Login } from "./pages";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import { Router } from "@reach/router";
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Layout>
-            <Route path="/" component={Home} />
-          </Layout>
-        </Switch>
-      </BrowserRouter>
-    </>
+    <Router>
+      <Login path="/login" />
+      <Layout path="/">
+        <Home path="/" />
+      </Layout>
+    </Router>
   );
 };
 
