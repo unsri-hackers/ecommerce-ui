e594a4265235148b6c237ac09cd2b71f660ee4ae
Aldi Anugra Pratama <aldianugra09@gmail.com>
2021-06-04 19:13:57 +0700

    Feature/3 login page (#3) (#23)
    * feat: add responsive login page (#3)
    
    * feat: add responsive login page (#3)
    
    * fix: remove header and footer from App.js
    
    * fix: spacing on desktop form (#22)
    
    * refactor: change directory of login form, and fix typo on font name (#3)
    
    * fix typo on croco.config.js

---
+++ src/pages/login/components/form/LoginForm.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Form, Input, Button, Divider } from "antd";
+import GoogleIcon from "../../../../assets/img/google-icon.png";
+import { Typography } from "antd";
+import "./login-form.less";
+
+const { Text, Link } = Typography;
+
+const LoginForm = () => {
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log(values);
+  };
+  return (
+    <div className="login-form">
+      <Form
+        layout="horizontal"
+        form={form}
+        requiredMark={false}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          label="Email Address"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "*Invalid email",
+              type: "email",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "*Invalid password",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item>
+          <div className="submit-button">
+            <Button type="primary" htmlType="submit" shape="round">
+              Login
+            </Button>
+          </div>
+        </Form.Item>
+      </Form>
+      <Divider className="login-devider" />
+      <Button
+        shape="round"
+        className="login-google"
+        icon={<img src={GoogleIcon} alt="G" />}
+      >
+        Login With Google
+      </Button>
+      <Link href="#" className="text-forgot-password">
+        Forgot Password?
+      </Link>
+      <Text className="text-register">
+        Haven't register yet? <Link href="#">Register Now</Link>
+      </Text>
+    </div>
+  );
+};
+
+export default LoginForm;
