88a3bb825378098c78da96809954901278bac3d4
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2021-06-04 20:42:44 +0700

    Add example on how to use React hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Router } from "@reach/router";
-import { Home, Login } from "./pages";
+import { Dummy, Home, Login } from "./pages";
 import Layout from "./components/layout/Layout";
 
 const PrivateRoute = ({ render, ...props }) => {
@@ -12,6 +12,8 @@ const App = () => {
   return (
     <Router>
       <Login path="/login" />
+
+      <Dummy path="/-/dummy" />
 
       <PrivateRoute path="/" render={Layout(Home)} />
     </Router>
---
+++ src/pages/dummy/Dummy.js
@@ -0,0 +1,46 @@
+import useRequest from "@ahooksjs/use-request";
+import React, { useEffect, useState } from "react";
+
+const Dummy = () => {
+  const [name, setName] = useState("arief");
+  const [status, setStatus] = useState("single");
+
+  const {
+    data: product1,
+    run: getProduct1
+  } = useRequest("https://deuvox.mocklab.io/api/v1/products/1", {
+    manual: true,
+  });
+
+  const { data: products } = useRequest("https://deuvox.mocklab.io/api/v1/products");
+
+  const buttonOnClick = () => {
+    setName("faisal");
+  }
+
+  useEffect(() => {
+    if (name === "faisal") {
+      setStatus("in relationship with amy");
+      getProduct1();
+    }
+  }, [name, getProduct1]);
+
+  return (
+    <>
+      <p>Product: {JSON.stringify(product1)}</p>
+      <p>List of product:</p>
+      <ul>
+        {products && products.map(product => (
+          <ol>{product.name} - Rp {product.price}</ol>
+        ))}
+      </ul>
+
+      <p>
+        {name} - {status}
+      </p>
+      <button onClick={() => buttonOnClick()}>click me</button>
+    </>
+  );
+};
+
+export default Dummy;
