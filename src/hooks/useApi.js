7cd6c63bf7b3681aa0d647304a2309f3d3bb38a6
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2021-06-05 20:14:19 +0700

    Add useApi hook

---
+++ src/hooks/useApi.js
@@ -0,0 +1,24 @@
+import useRequest from "@ahooksjs/use-request";
+import config from "../config";
+
+// useApi hook wraps ahooks' useRequest.
+// It can return mock response by specify REACT_APP_USE_MOCK_DATA to true
+// and params.mock to mock object.
+export const useApi = (
+  service,
+  params,
+) => {
+  const mockData = async file => {
+    return new Response(JSON.stringify(file), {
+      status: 200,
+      headers: { "Content-Type": "application/json" }
+    });
+  };
+
+  return useRequest(service, {
+    ...params,
+    requestMethod: config.useMockData
+      ? () => mockData(params.mock).then(response => response.json())
+      : null,
+  });
+};
---
+++ src/mocks/index.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  product: require("./data/product"),
+  productList: require("./data/productList"),
+}
