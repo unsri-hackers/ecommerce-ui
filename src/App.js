ad385cf9eb8791e513c46625cbe788494d1f6167
LordAldi <aldianugra09@gmail.com>
2021-06-07 19:52:05 +0700

    feat: configure google analytics (#10)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React from "react";
-import { Router } from "@reach/router";
+import { Router, Location, LocationProvider } from "@reach/router";
 import { Dummy, Home, Login } from "./pages";
 import Layout from "./components/layout/Layout";
+import ReactGA from "react-ga";
 
 const PrivateRoute = ({ render, ...props }) => {
   // TODO: Authentication logic goes here.
   return render(props);
 };
 
+ReactGA.initialize("UA-198873805-1", {
+  testMode: process.env.NODE_ENV === "test",
+});
+
+const trackPageView = (location) => {
+  ReactGA.pageview(location.pathname + location.search);
+};
+
 const App = () => {
   return (
-    <Router>
-      <Login path="/login" />
+    <LocationProvider>
+      <Router>
+        <Login path="/login" />
 
-      <Dummy path="/-/dummy" />
+        <Dummy path="/-/dummy" />
 
-      <PrivateRoute path="/" render={Layout(Home)} />
-    </Router>
+        <PrivateRoute path="/" render={Layout(Home)} />
+      </Router>
+      <Location children={(context) => trackPageView(context.location)} />
+    </LocationProvider>
   );
 };
 
