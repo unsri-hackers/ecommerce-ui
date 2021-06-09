197080a683c9d2e5f462b92580b479f51b8c5943
FadellMuhammad <fadel54031@gmail.com>
2021-06-09 19:53:39 +0700

    fix header responsive mobile and startedPage image

--- src/pages/startedPage/StartedPage.js
+++ src/pages/startedPage/StartedPage.js
@@ -9,15 +9,15 @@ const StartedPage = () => {
     return (
         <div className="startedPage">
             <Row align="middle">
-                <Col sm={24} md={15}>
+                <Col sm={24} md={24} lg={14}>
                     <div className="description">
                         <h1>Building successful <br /> business just got easier!</h1>
                         <p>You can count on us in making your business go <br /> up and up. Start selling your fascinating <br />products now.</p>
                         <Button id="button">Start Now</Button>
                     </div>
                 </Col>
-                <Col md={9}>
-                    <img alt="ilustration-online-shopping" src={ilustrationOnlineShopping} id="image-ilustration" />
+                <Col lg={10}>
+                    <img alt="ilustration-online-shopping" src={ilustrationOnlineShopping} id="image-ilustration"/>
                 </Col>
             </Row>
         </div>
