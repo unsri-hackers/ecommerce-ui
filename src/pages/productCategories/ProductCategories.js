31fe06f73d29ecae5519e16f80629c2cd0826cf8
FadellMuhammad <fadel54031@gmail.com>
2021-06-20 20:59:50 +0700

    update revisi

--- src/pages/productCategories/ProductCategories.js
+++ src/pages/productCategories/ProductCategories.js
@@ -1,25 +1,18 @@
 import useRequest from "@ahooksjs/use-request";
-import { Col, Row, Card } from "antd";
+import { Col, Row } from "antd";
 import "./ProductCategories.less";
+import Category from "./component/Category";
 
 const ProductCategories = () => {
     const { data: productCategories } = useRequest("https://4rvey.mocklab.io/api/category");
 
     return (
-        <div id="productCategory">
+        <div className="productCategory">
             <div className="container">
                 <Row>
-                    {productCategories && productCategories.map(category => (
-                        <Col>
-                            <Card
-                                hoverable
-                                cover={<img alt={category.name} src={category.image} />}
-                                id="card"
-                            >
-                            <div className="mask">
-                                <p id="title">{category.name}</p>
-                            </div>
-                            </Card>
+                    {productCategories && productCategories.map((category,i) => (
+                        <Col key={`category${i}`}>
+                            <Category data={category} />  
                         </Col>
                     ))}
                 </Row>
