6a00832f4a3a62dfd3c2dce6dad69776ceb6ca05
fadelarmando <fadelarmando3@gmail.com>
2021-06-25 00:23:59 +0700

    fix: add link in start button

--- src/pages/startedPage/StartedPage.js
+++ src/pages/startedPage/StartedPage.js
@@ -1,8 +1,10 @@
-import React, { useContext} from 'react';
+import React, { useContext } from 'react';
 import 'antd/dist/antd.css';
 import { Col, Row, Button } from "antd";
 import SiteContext from "../../providers/site/SiteContext";
 import ilustrationOnlineShopping from "../../assets/img/illustration-online-shopping.png";
+import { Link } from '@reach/router'
+
 
 import "./startedPage.less";
 
@@ -16,11 +18,13 @@ const StartedPage = () => {
                     <div className="description">
                         <h1>Building successful <br /> business just got easier!</h1>
                         <p>You can count on us in making your business go <br /> up and up. Start selling your fascinating <br />products now.</p>
-                        <Button id="button">Start Now</Button>
+                        <Link to="/products">
+                            <Button id="button">Start Now</Button>
+                        </Link>
                     </div>
                 </Col>
                 <Col lg={10}>
-                    <img alt="ilustration-online-shopping" src={ilustrationOnlineShopping} id="image-ilustration" width={ isMobile ? 200 : 400 }/>
+                    <img alt="ilustration-online-shopping" src={ilustrationOnlineShopping} id="image-ilustration" width={isMobile ? 200 : 400} />
                 </Col>
             </Row>
         </div>
