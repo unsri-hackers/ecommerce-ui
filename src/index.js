383392f8850290e97510820c570dae2c6813bc44
Aldi Anugra Pratama <aldianugra09@gmail.com>
2021-06-25 19:41:53 +0700

    faet: configure sentry.io (#55)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
-
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from "./App";
 
 import "./assets/less/index.less";
 
+Sentry.init({
+  dsn: "https://927ef7ca00694a1390b97c2ae2827c90@o879069.ingest.sentry.io/5831438",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
